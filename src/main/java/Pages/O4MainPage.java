5026997b1361e0f0f0665781e3e766640f1a5bd6
Roma <reva.htp@gmail.com>
2016-06-10 19:00:26 +0300

    test

--- src/main/java/Pages/O4MainPage.java
+++ src/main/java/Pages/O4MainPage.java
@@ -2,6 +2,7 @@ package Pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 
 /**
@@ -12,5 +13,10 @@ public class O4MainPage extends O0AbstractPage{
     public O4MainPage(WebDriver drv) {
         super(drv);
     }
+
+    public String checkUserName() {
+        return drv.findElement(By.xpath("//div[2]/div/div[2]")).getText();
+    }
+
 
 }
--- src/test/java/AutorizationTest.java
+++ src/test/java/AutorizationTest.java
@@ -1,6 +1,7 @@
 import Pages.O1AutorizationPage;
 import Pages.O2RegistrationPage;
 import Pages.O4MainPage;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 /**
@@ -9,9 +10,11 @@ import org.testng.annotations.Test;
 public class AutorizationTest extends AbstractTest {
 
     @Test
-    public void shouldSendNameInField() {
+    public void checkCorrectStandartLogin() {
         O1AutorizationPage onAuthPage = new O1AutorizationPage(drv);
         onAuthPage.navigateToWebApp();
         onAuthPage.fillCorrectForm().clickOnLogin();
+        O4MainPage onMainPage = new O4MainPage(drv);
+        Assert.assertTrue(onMainPage.checkUserName().equalsIgnoreCase("qwerast sadast"));
     }
 }
